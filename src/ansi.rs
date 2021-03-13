ec004811697efffdf5fe1df4978ba188787c9bc8
eddieshan <6906040+eddieshan@users.noreply.github.com>
2021-03-13 20:36:44 +0100

    ANSI escapes

---
+++ src/ansi.rs
@@ -0,0 +1,6 @@
+pub const NEXT_LINE: &[u8] = b"\x1b[1E";
+pub const HOME: &[u8] = b"\x1b[H";
+pub const CLEAR: &[u8] = b"\x1b[2J";
+pub const SET_FG: &[u8] = b"\x1b[38;2;";
+pub const SET_BG: &[u8] = b"\x1b[48;2;";
+pub const RESET: &[u8] = b"\x1b[0m";
