0d6f8294fca5b5dea5b438ee1936b95a8b17246f
eddieshan <6906040+eddieshan@users.noreply.github.com>
2021-03-13 19:53:15 +0100

    Types needed for console configuration

--- src/win/bindings.rs
+++ src/win/bindings.rs
@@ -11,3 +11,37 @@ pub type HANDLE = *mut c_void;
 pub type LPVOID = *mut c_void;
 pub type WCHAR = wchar_t;
 pub type LPCWSTR = *const WCHAR;
+
+#[repr(C)]
+pub struct SECURITY_ATTRIBUTES {
+    nLength: DWORD,
+    lpSecurityDescriptor: LPVOID,
+    bInheritHandle: BOOL
+}
+
+pub type LPSECURITY_ATTRIBUTES = *mut SECURITY_ATTRIBUTES;
+
+#[repr(C)]
+pub struct COORD {
+    pub X: SHORT,
+    pub Y: SHORT
+}
+
+#[repr(C)]
+pub struct SMALL_RECT {
+    pub Left: SHORT,
+    pub Top: SHORT,
+    pub Right: SHORT,
+    pub Bottom: SHORT
+}
+
+#[repr(C)]
+pub struct CONSOLE_SCREEN_BUFFER_INFO {
+    pub dwSize: COORD,
+    pub dwCursorPosition: COORD,
+    pub wAttributes: WORD,
+    pub srWindow: SMALL_RECT,
+    pub dwMaximumWindowSize: COORD
+}
+
+pub type PCONSOLE_SCREEN_BUFFER_INFO = *mut CONSOLE_SCREEN_BUFFER_INFO;
