dafe6be0edfbce3bed09d24fdd1743b6a5b5644e
eddieshan <devyalowda@gmail.com>
2022-02-05 16:20:24 +0100

    Clean up module dependencies

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,20 @@ mod settings;
 mod theme;
 mod cursor;
 
-mod win {
-    pub mod bindings;
-    pub mod term;
-}
-
-mod components {
-    pub mod status_bar;
-    pub mod gutter;
-}
+mod term;
+mod components;
 
 mod gap_buffer;
 mod editor;
 
 use std::io::Result;
 
-use crate::win::term;
 use crate::editor::Editor;
 
 fn main() -> Result<()> {
 
     let state = term::configure()?;
-    let mut editor = Editor::new(state);
+    let mut editor = Editor::new(&state);
     
     editor.run()
 }
