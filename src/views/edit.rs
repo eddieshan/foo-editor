f1ae04a955218afb6d4f024125a1017f1c2df0b1
eddieshan <devyalowda@gmail.com>
2022-02-08 18:21:12 +0100

    New default edit view

---
+++ src/views/edit.rs
@@ -0,0 +1,22 @@
+use std::io::{Result, Write};
+
+use crate::core::geometry::Position;
+use crate::term::vt100::*;
+use crate::models::editor::EditorState;
+use super::gutter;
+use super::status_bar;
+use crate::config::settings;
+
+pub fn render(buffer: &mut impl Write, state: &EditorState) -> Result<()> {
+
+    let (total_ln, lncol) = state.buffer.dump(buffer)?;
+
+    gutter::render(buffer, lncol.y, total_ln)?;
+    status_bar::render(buffer, &lncol, &state.term_info)?;
+    
+    let screen_pos = Position { x: lncol.x + settings::GUTTER_WIDTH, y: lncol.y };
+    
+    buffer.pos(screen_pos.y, screen_pos.x)?;
+
+    Ok(())
+}
