465ed9ff181c84d8ecfc8526ea9c4968a79e2c51
eddieshan <devyalowda@gmail.com>
2022-02-13 20:01:09 +0100

    Clearer handling of PieceCursor cases

--- src/buffers/piece_chain.rs
+++ src/buffers/piece_chain.rs
@@ -41,23 +41,40 @@ pub struct PieceChain {
     pieces: Vec<Piece>
 }
 
-fn find_cursor(pos: usize, pieces: &Vec<Piece>) -> PieceCursor {
+// fn find_cursor(pos: usize, pieces: &Vec<Piece>) -> PieceCursor {
+//     let mut count = 0;
+//     let limit = pos + 1;
+
+//     for i in 0..pieces.len() {
+//         let piece = &pieces[i];
+//         let next_count = count + piece.size;
+//         if next_count == 0 {
+//             return PieceCursor { pos: 0, offset: 0 };
+//         } else if next_count < limit {
+//             count = next_count;
+//         } else {
+//             return PieceCursor { pos: i, offset: pos - count };
+//         }
+//     }
+
+//     PieceCursor { pos: pieces.len(), offset: 0 }
+// }
+
+fn find_cursor(pos: usize, pieces: &Vec<Piece>) -> Option<PieceCursor> {
     let mut count = 0;
     let limit = pos + 1;
 
     for i in 0..pieces.len() {
         let piece = &pieces[i];
         let next_count = count + piece.size;
-        if next_count == 0 {
-            return PieceCursor { pos: 0, offset: 0 };
-        } else if next_count < limit {
+        if next_count < limit {
             count = next_count;
         } else {
-            return PieceCursor { pos: i, offset: pos - count };
+            return Some(PieceCursor { pos: i, offset: pos - count });
         }
     }
 
-    PieceCursor { pos: pieces.len(), offset: 0 }
+    None
 }
 
 impl PieceChain {
@@ -70,16 +87,10 @@ impl PieceChain {
             pieces: pieces
         }
     }
-    
+
     pub fn insert(&mut self, val: u8, pos: usize) {
-        let end_pos = self.buffer.len();
-        if pos == end_pos {
-            if let Some(piece) = self.pieces.last_mut() {
-                piece.size += 1;
-            }
-        } else {
-            let cursor = find_cursor(pos, &self.pieces);
-            let new_piece = Piece { start: end_pos, size: 1 };
+        if let Some(cursor) = find_cursor(pos, &self.pieces) {
+            let new_piece = Piece { start: self.buffer.len(), size: 1 };
 
             if cursor.offset == 0 {
                 self.pieces.insert(cursor.pos, new_piece);
@@ -92,11 +103,16 @@ impl PieceChain {
 
                 self.pieces.insert(new_pos, piece_right);
                 self.pieces.insert(new_pos, new_piece);
-            }
+            }            
+        } else if self.size() == self.buffer.len() {
+            let piece = self.pieces.last_mut().unwrap();
+            piece.size += 1;
+        } else {
+            self.pieces.push(Piece { start: self.buffer.len(), size: 1 });
         }
 
         self.buffer.push(val);
-    }
+    }    
 
     pub fn append(&mut self, data: &[u8]) {
         if let Some(piece) = self.pieces.last_mut() {
@@ -106,26 +122,29 @@ impl PieceChain {
         }
     }
 
+    fn size(&self) -> usize {
+        self.pieces.last().map_or(0, |p| p.start + p.size)
+    }
+
     pub fn erase(&mut self, pos: usize) {
-        if pos < self.buffer.len() {
-            let cursor = find_cursor(pos, &self.pieces);
+        if let Some(cursor) = find_cursor(pos, &self.pieces) {
             let n_pieces = self.pieces.len();
             let piece = &mut self.pieces[cursor.pos];
 
-            if piece.size == 1 && n_pieces > 1 {
-                self.pieces.remove(cursor.pos);
-            } else if piece.size > 0 {
-                if cursor.offset == 0 {
-                    piece.shrink_right();
-                } else if cursor.offset == piece.size - 1 {
-                    piece.shrink_left();
-                } else {
-                    let piece_right = piece.split_right(cursor.offset + 1);
-                    piece.resize(cursor.offset);
-        
-                    let new_pos = cursor.pos + 1;
-                    self.pieces.insert(new_pos, piece_right);    
-                }
+            if piece.size > 0 {
+                match (piece.size, n_pieces, cursor.offset) {
+                    (1, 1, _) => piece.shrink_left(),
+                    (1, _, _) => { self.pieces.remove(cursor.pos); },
+                    (_, _, 0) => piece.shrink_right(),
+                    (_, _, offset) if offset == piece.size - 1 => piece.shrink_left(),
+                    (_, _, _) => {
+                        let piece_right = piece.split_right(cursor.offset + 1);
+                        piece.resize(cursor.offset);
+            
+                        let new_pos = cursor.pos + 1;
+                        self.pieces.insert(new_pos, piece_right);                    
+                    }
+                };
             }
         }
     }
