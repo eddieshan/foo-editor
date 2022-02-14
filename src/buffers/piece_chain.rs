fb14b6db3767bf44e65a74dfa7ccb80c5d59a831
eddieshan <devyalowda@gmail.com>
2022-02-14 15:16:25 +0100

    Rename piece chain

--- src/buffers/piece_chain.rs
+++ src/buffers/piece_chain.rs
@@ -42,7 +42,7 @@ struct PieceCursor {
 
 pub struct PieceChain {
     buffer: Vec<u8>,
-    pieces: Vec<Piece>
+    chain: Vec<Piece>
 }
 
 fn find_cursor(pos: usize, pieces: &Vec<Piece>) -> Option<PieceCursor> {
@@ -63,34 +63,34 @@ fn find_cursor(pos: usize, pieces: &Vec<Piece>) -> Option<PieceCursor> {
 impl PieceChain {
 
     pub fn with_capacity(capacity: usize, n_pieces: usize) -> Self {
-        let mut pieces = Vec::with_capacity(n_pieces);
-        pieces.push(Default::default());
+        let mut chain = Vec::with_capacity(n_pieces);
+        chain.push(Default::default());
         PieceChain {
             buffer: Vec::with_capacity(capacity),
-            pieces: pieces
+            chain: chain
         }
     }
 
     pub fn insert(&mut self, val: u8, pos: usize) {
-        let last_pos = self.pieces.last().map_or(0, |p| p.start + p.size);
+        let last_pos = self.chain.last().map_or(0, |p| p.start + p.size);
         let size = self.buffer.len();
-        match (find_cursor(pos, &self.pieces), last_pos == size) {
-            (None, true)  => self.pieces.last_mut().unwrap().extend(1),
-            (None, false) => self.pieces.push(Piece { start: size, size: 1 }),
+        match (find_cursor(pos, &self.chain), last_pos == size) {
+            (None, true)  => self.chain.last_mut().unwrap().extend(1),
+            (None, false) => self.chain.push(Piece { start: size, size: 1 }),
             (Some(cursor), _) => {
                 let new_piece = Piece { start: size, size: 1 };
 
                 if cursor.offset == 0 {
-                    self.pieces.insert(cursor.pos, new_piece);
+                    self.chain.insert(cursor.pos, new_piece);
                 } else {
-                    let piece = &mut self.pieces[cursor.pos];
+                    let piece = &mut self.chain[cursor.pos];
                     let piece_right = piece.split_right(cursor.offset);
                     piece.resize(cursor.offset);
     
                     let new_pos = cursor.pos + 1;
     
-                    self.pieces.insert(new_pos, piece_right);
-                    self.pieces.insert(new_pos, new_piece);
+                    self.chain.insert(new_pos, piece_right);
+                    self.chain.insert(new_pos, new_piece);
                 }
             }
         }
@@ -99,21 +99,21 @@ impl PieceChain {
     }
 
     pub fn append(&mut self, data: &[u8]) {
-        if let Some(piece) = self.pieces.last_mut() {
+        if let Some(piece) = self.chain.last_mut() {
             piece.extend(data.len());
             self.buffer.extend_from_slice(data);
         }
     }
 
     pub fn erase(&mut self, pos: usize) {
-        if let Some(cursor) = find_cursor(pos, &self.pieces) {
-            let n_pieces = self.pieces.len();
-            let piece = &mut self.pieces[cursor.pos];
+        if let Some(cursor) = find_cursor(pos, &self.chain) {
+            let n_pieces = self.chain.len();
+            let piece = &mut self.chain[cursor.pos];
 
             if piece.size > 0 {
                 match (piece.size, n_pieces, cursor.offset) {
                     (1, 1, _) => piece.shrink_left(),
-                    (1, _, _) => { self.pieces.remove(cursor.pos); },
+                    (1, _, _) => { self.chain.remove(cursor.pos); },
                     (_, _, 0) => piece.shrink_right(),
                     (_, _, offset) if offset == piece.size - 1 => piece.shrink_left(),
                     (_, _, _) => {
@@ -121,7 +121,7 @@ impl PieceChain {
                         piece.resize(cursor.offset);
             
                         let new_pos = cursor.pos + 1;
-                        self.pieces.insert(new_pos, piece_right);                    
+                        self.chain.insert(new_pos, piece_right);                    
                     }
                 };
             }
@@ -130,8 +130,8 @@ impl PieceChain {
 
     pub fn clear(&mut self) {
         self.buffer.clear();
-        self.pieces.clear();
-        self.pieces.push(Default::default());
+        self.chain.clear();
+        self.chain.push(Default::default());
     }
 
     pub fn capacity(&self) -> usize {
@@ -140,7 +140,7 @@ impl PieceChain {
 
     pub fn copy_to(&self, buffer: &mut Vec<u8>) {
         buffer.clear();
-        for piece in self.pieces.iter() {
+        for piece in self.chain.iter() {
             let end = piece.start + piece.size;
             buffer.extend_from_slice(&self.buffer[piece.start..end]);
         }
