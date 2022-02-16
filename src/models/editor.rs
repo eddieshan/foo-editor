015a23dcdb5d4d8b0c53f3bc8edc8a5de1f8dd24
eddieshan <devyalowda@gmail.com>
2022-02-16 23:30:00 +0100

    Fix inconsistency between relative and absolute positioning

--- src/models/editor.rs
+++ src/models/editor.rs
@@ -1,3 +1,4 @@
+use std::ops::Range;
 use crate::core::geometry::Size;
 use crate::buffers::piece_chain::PieceChain;
 use crate::core::utils::AbsPos;
@@ -14,7 +15,7 @@ impl Region {
         if new_pos > self.start {
             let n_lines = navigation::n_lines(&text[self.start..new_pos]);
             if n_lines > page_size {
-                self.start = text.rapos_n(LF, page_size - 1, new_pos).unwrap_or(0);
+                self.start = text.rapos_n(LF, page_size - 1, new_pos).map_or(0, |v| v + 1);
             }
         } else if new_pos < self.start {
             self.start = text.rapos(LF, new_pos).unwrap_or(0);
@@ -23,9 +24,13 @@ impl Region {
         self.pos = new_pos;
     }
 
-    pub fn clip<'a>(&self, text: &'a [u8], page_size: usize) -> &'a [u8] {
+    pub fn clip<'a>(&self, text: &'a [u8], page_size: usize) -> (Range<usize>, &'a [u8]) {
         let end = text.apos_n(LF, page_size, self.start).unwrap_or(text.len());
-        &text[self.start..end]
+        let lines_range = Range {
+            start: navigation::n_lines(&text[0..self.start]),
+            end: navigation::n_lines(&text[0..end]) + 1
+        };
+        (lines_range, &text[self.start..end])
     }
 }
 
@@ -33,7 +38,7 @@ pub struct EditorState {
     pub window_size: Size,
     buffer: PieceChain,
     pub text: Vec<u8>,
-    pub cursor: Region
+    pub region: Region
 }
 
 impl EditorState {
@@ -47,7 +52,7 @@ impl EditorState {
             window_size: window_size,
             buffer: PieceChain::with_capacity(buffer_size, n_pieces),
             text: Vec::with_capacity(screen_buffer_size),
-            cursor: Region { start: 0, pos: 0 }
+            region: Region { start: 0, pos: 0 }
         }
     }
 
@@ -59,15 +64,15 @@ impl EditorState {
     }
 
     pub fn insert(&mut self, val: u8) {
-        self.buffer.insert(val, self.cursor.pos);
+        self.buffer.insert(val, self.region.pos);
         self.refresh();
-        self.cursor.update(&self.text, self.cursor.pos + 1, self.window_size.height);
+        self.region.update(&self.text, self.region.pos + 1, self.window_size.height);
     }
 
     pub fn erase(&mut self) {
-        self.buffer.erase(self.cursor.pos);
+        self.buffer.erase(self.region.pos);
         self.refresh();
-        self.cursor.update(&self.text, self.cursor.pos, self.window_size.height);
+        self.region.update(&self.text, self.region.pos, self.window_size.height);
     }
 
     pub fn go_erase(&mut self, mv: fn(&[u8], usize) -> usize) {
@@ -76,7 +81,7 @@ impl EditorState {
     }
 
     pub fn go_to(&mut self, mv: fn(&[u8], usize) -> usize) {
-        let new_pos = mv(&self.text, self.cursor.pos);
-        self.cursor.update(&self.text, new_pos, self.window_size.height);
+        let new_pos = mv(&self.text, self.region.pos);
+        self.region.update(&self.text, new_pos, self.window_size.height);
     }
 }
--- src/views/edit.rs
+++ src/views/edit.rs
@@ -8,18 +8,18 @@ use crate::models::editor::EditorState;
 use super::{plain_text, gutter, status_bar};
 
 pub fn render(buffer: &mut impl Write, state: &EditorState) -> Result<(), EditorError> {
-    let n_lines = navigation::n_lines(&state.text);
-    let cursor = navigation::cursor(&state.text[0..state.cursor.pos]);
+    let absolute_pos = navigation::cursor(&state.text[..state.region.pos]);
+    let (lines_range, text) = state.region.clip(&state.text, state.window_size.height - 1);
 
-    let text = state.cursor.clip(&state.text, state.window_size.height - 1);
     if text.len() > 0 {
         plain_text::render(buffer, text)?;
     }
 
-    gutter::render(buffer, cursor.y, n_lines)?;
-    status_bar::render(buffer, &cursor, &state.window_size)?;
-    
-    let screen_pos = Position { x: cursor.x + settings::GUTTER_WIDTH, y: cursor.y };
+    gutter::render(buffer, absolute_pos.y, lines_range)?;
+    status_bar::render(buffer, &absolute_pos, &state.window_size)?;
+
+    let relative_pos = navigation::cursor(&state.text[state.region.start..state.region.pos]);
+    let screen_pos = Position { x: relative_pos.x + settings::GUTTER_WIDTH, y: relative_pos.y };
     
     buffer.pos(screen_pos.y, screen_pos.x)?;
 
