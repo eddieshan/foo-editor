6da68acc966f7bf8c3f0d47b79b58a8b65ddc93e
eddieshan <devyalowda@gmail.com>
2022-02-17 19:37:11 +0100

    More robust text slice boundary handling

--- src/views/edit.rs
+++ src/views/edit.rs
@@ -9,10 +9,7 @@ use super::{plain_text, line_counter, status_bar};
 pub fn render(buffer: &mut impl Write, state: &EditorState) -> Result<(), EditorError> {
     let layout = state.layout(state.window_size.height - 1);
 
-    if layout.text.len() > 0 {
-        plain_text::render(buffer, layout.text)?;
-    }
-
+    plain_text::render(buffer, layout.text)?; 
     line_counter::render(buffer, layout.cursor.abs.y, layout.lines_range)?;
     status_bar::render(buffer, &layout.cursor.abs, &state.window_size)?;
 
