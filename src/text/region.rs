05d162b487fe18f0549c1002db5d283391116fea
eddieshan <devyalowda@gmail.com>
2022-02-20 18:15:04 +0100

    Fix incorrect region indices

--- src/text/region.rs
+++ src/text/region.rs
@@ -8,8 +8,8 @@ pub struct Region {
 
 impl Region {
     pub fn update(&mut self, text: &[u8], new_pos: usize) {
-        if new_pos > self.start && (&text[self.start..new_pos]).at_least(LF, self.page_size + 1) {
-            self.start = text.rpos_n(LF, self.page_size - 1, new_pos).map_or(0, |v| v + 1);
+        if new_pos > self.start && (&text[self.start..new_pos]).at_least(LF, self.page_size) {
+            self.start = text.rpos_n(LF, self.page_size, new_pos).map_or(0, |v| v + 1);
         } else if new_pos < self.start {
             self.start = text.rpos(LF, new_pos).unwrap_or(0);
         }
